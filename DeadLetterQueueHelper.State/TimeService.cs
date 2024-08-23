6dd4f362ff8743070578e860ff8666c72e805404
Jonathan Svahn <jonathan.svahn@indevit.fi>
2024-08-23 21:52:43 +0300

    Add basics

---
+++ DeadLetterQueueHelper.State/TimeService.cs
@@ -0,0 +1,28 @@
+﻿using Stl.Fusion;
+
+namespace DeadLetterQueueHelper.State
+{
+    public class TimeService : IComputeService
+    {
+        private readonly TimeProvider _timeProvider;
+
+        public TimeService(TimeProvider timeProvider)
+        {
+            _timeProvider = timeProvider;
+        }
+
+        [ComputeMethod]
+        public Task<DateTimeOffset> GetTime()
+        {
+            return Task.FromResult(_timeProvider.GetLocalNow());
+        }
+
+        public void Refresh()
+        {
+            using (Computed.Invalidate())
+            {
+                _ = GetTime();
+            }
+        }
+    }
+}
--- DeadLetterQueueHelper/Program.cs
+++ DeadLetterQueueHelper/Program.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DeadLetterQueueHelper;
+using Stl.Fusion;
+using DeadLetterQueueHelper.State;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFusion(Stl.Rpc.RpcServiceMode.None);
+
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddSingleton(TimeProvider.System)
+    .AddSingleton<TimeService>();
 
 builder.Services.AddMsalAuthentication(options =>
 {

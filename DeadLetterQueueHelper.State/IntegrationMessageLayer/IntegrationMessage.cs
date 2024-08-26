602b26ca17c1df440967b7a58345c294a90afbb4
Jonathan Svahn <jonathan.svahn@indevit.fi>
2024-08-26 18:03:57 +0300

    browse attempts in the message, resubmit any

--- DeadLetterQueueHelper.State/IntegrationMessageLayer/IntegrationMessage.cs
+++ DeadLetterQueueHelper.State/IntegrationMessageLayer/IntegrationMessage.cs
@@ -1,10 +1,4 @@
 ﻿using Azure.Messaging.ServiceBus;
-using Stl.Fusion;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DeadLetterQueueHelper.State.IntegrationMessageLayer
 {
@@ -13,7 +7,5 @@ namespace DeadLetterQueueHelper.State.IntegrationMessageLayer
         public string Id => Attempts.First().MessageId;
         public string Subject => Attempts.First().Subject;
         public string FirstEnqueuedTime => Attempts.First().EnqueuedTime.ToLocalTime().ToString();
-        public string DeadLetterReason => Attempts.First().DeadLetterReason;
-        public string Body => Attempts.First().Body.ToString();
     }
 }
